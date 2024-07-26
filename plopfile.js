07dd00e6ca1f7cf05ea86255e7499d0b2de7de25
Don Hamilton III <donhamiltoniii@gmail.com>
2024-07-26 05:28:17 -0400

    feat(plop): add plop to project
    - add plop and blog template
    - update date format and add util

---
+++ plopfile.js
@@ -0,0 +1,34 @@
+export default function (plop) {
+  plop.setHelper("todayString", function () {
+    const date = new Date();
+
+    return date.toLocaleString("en-GB", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+  });
+  // create your generators here
+  plop.setGenerator("blog", {
+    description: "generates a new blog post file",
+    prompts: [
+      {
+        type: "input",
+        name: "title",
+        message: "What are you calling this monster?: ",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Tell me a little bit about it: ",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/content/blog/{{kebabCase title}}.js",
+        templateFile: "src/plop-templates/new-blog-post.hbs",
+      },
+    ],
+  });
+}
