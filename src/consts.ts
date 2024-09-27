51897e3f29e8927ed25d6907edf2e4d9edd5f70c
Don Hamilton III <donhamiltoniii@gmail.com>
2024-09-27 10:30:01 -0400

    refactor: move image path to constant

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "DONDON Develops";
 export const SITE_DESCRIPTION =
   "Thoughts, walkthroughs, and explanations of concepts related to software development.";
+export const RANDOM_IMAGE = "https://picsum.photos/720/360";
