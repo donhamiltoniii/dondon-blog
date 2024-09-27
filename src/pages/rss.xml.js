2b4554f61d1f1286de31e722d0335a63020163c7
Don Hamilton III <donhamiltoniii@gmail.com>
2024-09-27 10:59:18 -0400

    refactor: replace createDate with pubDate
    - astro RSS needs `pubDate` as a property instead of a custom name

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,16 +4,18 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function get(context) {
   const posts = await getCollection("blog");
+  const items = posts
+    .filter((post) => post.data.published)
+    .map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    }));
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     trailingSlash: false,
-    items: posts
-      .filter((post) => post.data.published)
-      .map((post) => ({
-        ...post.data,
-        link: `/blog/${post.slug}/`,
-      })),
+    items,
   });
 }
