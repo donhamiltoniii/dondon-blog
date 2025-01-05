47680cd52f043115f303ee5a2017104c08b18538
Don Hamilton III <donhamiltoniii@gmail.com>
2025-01-05 07:43:14 -0500

    chore: replace altered function param

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -1,7 +1,7 @@
 import { execSync } from "child_process";
 
 export function remarkModifiedTime() {
-  return function (_tree, file) {
+  return function (tree, file) {
     const filepath = file.history[0];
     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastUpdate = result.toString();
