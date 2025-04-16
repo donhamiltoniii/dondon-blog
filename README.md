901a0c59f92839f649c0eca0d3565a8b6512e05c
Don Hamilton III <donhamiltoniii@gmail.com>
2025-04-16 16:28:30 -0400

    feat: add gallery page and logic

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # DONDON BLOG
+
+## Content
+
+### Blog
+
+### Gallery
+
+Gallery content is mapped as follows:
+
+```
+src
+|- content/
+|——— albums/
+|————— hiking-trip/
+|——————— cover.jpg
+|——————— photo1.jpg
+|——————— photo2.jpg
+|————— some-other-album/
+|——————— cover.jpg
+|——————— photo1.jpg
+|——————— photo2.jpg
+|——— config.ts
+```
--- src/content/config.ts
+++ src/content/config.ts
@@ -19,4 +19,14 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const albums = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      cover: image(),
+    }),
+});
+
+export const collections = { blog, albums };
