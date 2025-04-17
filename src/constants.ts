aaaa1f37d97cb7ec2dd0326ee6375e6766e622e6
Don Hamilton III <donhamiltoniii@gmail.com>
2025-04-17 14:57:20 -0400

    feat: remove hero image

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
 export const SITE_TITLE = "Dondon Developments";
 export const SITE_DESCRIPTION =
   "A digital garden space for me to share writing, photos, musings, tutorial, and educational materials";
-export const RANDOM_IMAGE_URL = "https://picsum.photos/720/360";
